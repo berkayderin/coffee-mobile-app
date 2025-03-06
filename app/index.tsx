d91181ecb20d20edc3eec8290fd583bc76caa0fd
Berkay Derin <zewodi@gmail.com>
2025-03-06 15:28:35 +0300

    feat: add link expo-router

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
 import icedCoffeeImg from '@/assets/images/iced-coffee.png'
+import { Link } from 'expo-router'
 
 const app = () => {
 	return (
@@ -9,11 +10,15 @@ const app = () => {
 				source={icedCoffeeImg}
 				className="flex-1"
 				resizeMode="cover"
-				imageStyle={{ opacity: 0.5 }}
 			>
 				<Text className="font-semibold text-2xl text-center bg-black text-white">
 					coffee shop
 				</Text>
+				<Link href="/explore" className="mt-10">
+					<Text className="bg-red-500 font-semibold text-2xl text-center text-white">
+						Explore Go
+					</Text>
+				</Link>
 			</ImageBackground>
 		</View>
 	)
